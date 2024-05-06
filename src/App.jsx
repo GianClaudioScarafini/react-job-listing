f59e33cffa9db8e6e5dad4f24cc2d02a9c8bf84c
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-06 10:08:42 +0100

    chore: Update App.jsx to use react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Router,
+} from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
-import JobLIstings  from "./components/JobLIstings";
+import JobLIstings from "./components/JobLIstings";
 import ViewAllJobs from "./components/ViewAllJobs";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>my App</h1>} />)
+);
+
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobLIstings />
-      <ViewAllJobs />
-
-
-
-    </>
-  );
+  return  <RouterProvider router={router} /> ;
 };
 
 export default App;
