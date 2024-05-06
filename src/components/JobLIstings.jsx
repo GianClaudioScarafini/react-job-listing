c90753c7da86344a140486c32c8f6961cc34ffe4
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-06 15:08:19 +0100

    chore: Add server script to package.json for running json-server on port 8000

--- src/components/JobLIstings.jsx
+++ src/components/JobLIstings.jsx
@@ -1,12 +1,29 @@
 /* eslint-disable react/no-unknown-property */
+import { useState, useEffect } from 'react';
+
 import React from 'react'
-import jobs from '../../jobs.json'
+
 import JobListing from './JobListing'
 
 
 const JobLIsting = ({isHome = false}) => {
-    const jobListing = isHome ? jobs.slice(0, 3) : jobs;
-    console.log(jobListing)
+    const [jobs, setJobs ] = useState([]);
+    const [loding, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            
+            try {
+                const response = await fetch ('http:localhost:800/jobs');
+                const data = await response.json();
+                setJobs(data);
+            } catch (error) {
+                console.log('error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+    }, [])
     return (
         <section class="bg-blue-50 px-4 py-10">
             <div class="container-xl lg:container m-auto">
@@ -14,7 +31,7 @@ const JobLIsting = ({isHome = false}) => {
                     {isHome ? 'Featured Jobs' : 'All Jobs'}
                 </h2>
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobListing.map((job) => (
+                    {jobs.map((job) => (
                         <JobListing key={job.id} job={job}/>
                     ))}
                 </div>
