88cbb3cf060657c31a390be07bf2a80e23fb1548
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-06 11:26:08 +0100

    chore: Update Navbar component to use NavLink for navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/no-unknown-property */
 import React from 'react';
 import logo from '../assets/images/logo.png';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+
+    const linkClass =({isActive}) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+
     return (
         <div>
             <nav class="bg-indigo-700 border-b border-indigo-500">
@@ -12,7 +15,7 @@ export default function Navbar() {
                         <div
                             class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
                         >
-                            <Link class="flex flex-shrink-0 items-center mr-4" to="/index.html">
+                            <NavLink class="flex flex-shrink-0 items-center mr-4" to="/index.html">
                                 <img
                                     class="h-10 w-auto"
                                     src={logo}
@@ -21,24 +24,23 @@ export default function Navbar() {
                                 <span class="hidden md:block text-white text-2xl font-bold ml-2"
                                 >React Jobs</span
                                 >
-                            </Link>
+                            </NavLink>
                             <div class="md:ml-auto">
                                 <div class="flex space-x-2">
-                                    <Link
+                                    <NavLink
                                         to="/"
-                                        class="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                    >Home</Link
-                                    >
-                                    <Link
+                                        className={linkClass}
+                                    >Home
+                                    </NavLink>
+                                    <NavLink
                                         to="/jobs"
-                                        class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                    >Jobs</Link
+                                        className={linkClass}
+                                    >Jobs</NavLink
                                     >
-                                    <Link
+                                    <NavLink
                                         to="/add-job"
-                                        class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                    >Add Job</Link
-                                    >
+                                        className={linkClass}
+                                    >Add Job</NavLink>
                                 </div>
                             </div>
                         </div>
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import JobListing from '../components/JobLIstings'
 
 const JobsPage = () => {
   return (
     <>
-        <div>JobsPage</div>
+        <secton className="bg-blue-50 px-4 py-6">
+            <JobListing />
+        </secton>
     </>
   )
 }
