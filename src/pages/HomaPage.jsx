ecee29c099c52aa5d440e36fe363126acc9ffd1a
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-06 10:47:17 +0100

    chore: Add JobsPage component and update routing in App.jsx

--- src/pages/HomaPage.jsx
+++ src/pages/HomaPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import JobListings from '../components/JobLIstings';  
+import ViewAllJobs from '../components/ViewAllJobs';
+
 
 
 const HomaPage = () => {
     return (
         <div>
             <Hero />
+            <HomeCards />
+            <JobListings />
+            <ViewAllJobs />
         </div>
     )
 }
