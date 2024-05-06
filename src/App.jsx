da50111582058c9950962b8c3eb696ab411c3a25
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-06 09:50:15 +0100

    start the project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobLIstings  from "./components/JobLIstings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobLIstings />
+      <ViewAllJobs />
+
+
+
+    </>
+  );
+};
+
+export default App;
