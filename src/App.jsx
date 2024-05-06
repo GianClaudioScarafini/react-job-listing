c54934ef9425d9c550c3e0a2f4f7845e43b53efb
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-06 19:43:31 +0100

    Update AddJobPage component prop name to addJobSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/add-job' element={<AddJobPage addJobSubmeet={addJob} />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
         <Route path='*' element={<NotFounPage />} />
       </Route>
